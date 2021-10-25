103d41331cab0c999591ab0560fed317cc09c38a
Ivan M <vania.morskov@yandex.ru>
2021-10-25 22:15:23 +0300

    complete 2pages desktop versions

--- src/js/script.js
+++ src/js/script.js
@@ -1,5 +1,5 @@
-const [a, b] = [5, 9];
+// const [a, b] = [5, 9];
 
-const sum = (...args) => [...args].reduce((sum, elem) => sum + elem, 0);
+// const sum = (...args) => [...args].reduce((sum, elem) => sum + elem, 0);
 
-console.log(sum(a, b));
+// console.log(sum(a, b));
